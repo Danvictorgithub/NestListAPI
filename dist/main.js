c1e088e2098e3969ea315b81c98524f20e515d7c
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-02-25 12:27:13 +0800

    enabled cors and helmet

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const logger = require("morgan");
+const helmet_1 = require("helmet");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use(logger('dev'));
+    app.use((0, helmet_1.default)());
+    app.enableCors();
     await app.listen(8080);
 }
 bootstrap();
